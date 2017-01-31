6f893c7c670e50c19287e3b0ba588235a45dba79
João Vieira <joao@evrythng.com>
2017-01-31 14:01:43 +0000

    1.0.0-pre.1

---
+++ dist/evrythng-pubsub.es6.js
@@ -0,0 +1,18 @@
+/**
+ * EVRYTHNG-PUBSUB.JS v1.0.0-pre.0
+ * (c) 2012-2017 EVRYTHNG Ltd. London / New York / San Francisco.
+ * Released under the Apache Software License, Version 2.0.
+ * For all details and usage:
+ * https://github.com/evrythng/evrythng-pubsub.js
+ */
+
+import { Resource } from 'evrythng';
+
+Resource.subscribe = () => {
+  console.log('subscribing');
+};
+
+const version = '1.0.0';
+
+export { version };
+//# sourceMappingURL=evrythng-pubsub.es6.js.map
