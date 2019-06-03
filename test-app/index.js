2c55e0997338934b1aceae370cb9753c2d14a85d
C-D-Lewis <Bonsitm@gmail.com>
2019-06-03 13:24:34 +0100

    Improve test-app and add test-node

--- test-app/index.js
+++ test-app/index.js
@@ -1,1 +1,18 @@
 evrythng.use(PubSub);
+
+const getQueryParam = key => new URLSearchParams(window.location.search).get(key);
+
+const main = async () => {
+  const apiKey = getQueryParam('trustedApp');
+  if (!apiKey) {
+    alert('Please specify \'trustedApp\' query parameter');
+    return;
+  }
+
+  const trustedApp = new evrythng.TrustedApplication(apiKey);
+  await trustedApp.init();
+
+  window.trustedApp = trustedApp;
+};
+
+main();
---
+++ test-node/index.js
@@ -0,0 +1,22 @@
+const evrythng = require('evrythng')
+const PubSub = require('evrythng-pubsub')
+
+evrythng.use(PubSub)
+
+const { TRUSTED_API_KEY } = process.env
+
+const onMessage = message => console.log(JSON.stringify(message))
+
+const main = async () => {
+  if (!TRUSTED_API_KEY) {
+    throw new Error('Please export TRUSTED_API_KEY')
+  }
+
+  const trustedApp = new evrythng.TrustedApplication(TRUSTED_API_KEY)
+  await trustedApp.init()
+  
+  await trustedApp.action('all').subscribe(onMessage)
+  console.log(`Subscribed to all actions in project ${trustedApp.project}`)
+}
+
+main()
