c1202a67cdefd1a1f7692e235240bcec73973590
C-D-Lewis <Bonsitm@gmail.com>
2020-04-01 14:18:42 +0100

    Browser test app automatically lists actions

--- tests/browser/index.js
+++ tests/browser/index.js
@@ -2,6 +2,12 @@ evrythng.use(PubSub)
 
 const getQueryParam = key => new URLSearchParams(window.location.search).get(key)
 
+const addListItem = item => {
+  const li = document.createElement('li')
+  li.innerHTML = JSON.stringify(item)
+  document.getElementById('list').appendChild(li)
+}
+
 const main = async () => {
   const apiKey = getQueryParam('trustedApp')
   if (!apiKey) {
@@ -13,6 +19,8 @@ const main = async () => {
   await trustedApp.init()
 
   window.trustedApp = trustedApp
+
+  trustedApp.action('all').subscribe(addListItem)
 }
 
 main()
